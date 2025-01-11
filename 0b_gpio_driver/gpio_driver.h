87db4c2e8916d0fad3599434b7da5f2890f90e97
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-01-10 20:57:13 -0500

    Update gpio_driver.h
    Add commit message to explain <stdbool.h>

--- 0b_gpio_driver/gpio_driver.h
+++ 0b_gpio_driver/gpio_driver.h
@@ -1,8 +1,10 @@
 #ifndef _GPIO_DRIVER_H
 #define _GPIO_DRIVER_H
 
+// Include the function that allows the usage of booleans on the microcontroller.
 #include <stdbool.h>
 
+// Create void functions
 void p1_button_init(void);
 bool get_p13_button_state(void);
 
